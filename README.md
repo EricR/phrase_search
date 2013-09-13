8eb119d86a1fad4d7cd901f1953928e9a5e8f9aa
Eric Rafaloff <erafaloff@gmail.com>
2013-09-13 17:35:40 -0400

    Introduce Tokens and Documents

--- README.md
+++ README.md
@@ -3,46 +3,29 @@
 A small text searcher that is optimized to work with larger bodies of text.
 It:
 
-* Takes a paragraph and breaks it up into sentences using punctuation.
-* Tokenizes each sentence with a lowercase and nGrams tokenizer.
-* Stores the tokens in a records map, which makes sure a single body of
-  text won't create duplicate records.
-* Stores the records map in the index.
+* Stores Documents, which are  made up of a UUID and a body of text.
+* Breaks the body of text up into tokens using sentence, lowercase, and
+  nGrams tokenizers. These tokens get stored into a Token Map, which
+  makes sure duplicate tokens don't get added for the same document.
+* Tokens reference Documents.
+* Stores the tokens into the Index.
 
 ## Example
 
-```go
-index := NewIndex("Lorem Ipsums", false)
-
-fmt.Printf("\nWriting lorem ipsums to 'facts'\n\n")
-index.Add("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. A needle in a hay stack", "First Lorem Ipsum")
-index.Add("Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim.", "Second Loreum Ipsum")
-index.Add("Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit.", "Third Loreum Ipsum")
-index.Add("Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus, accumsan porttitor, cursus quis, aliquet eget, justo. ", "Fourth Loreum Ispum")
-
-fmt.Printf("\nSearching for 'a needle in a Hay Stack' in 'Lorem Ipsums' index")
-time_start := time.Now()
-needle := index.Search("a needle in a Hay Stack")
-time_total := time.Now().Sub(time_start).Seconds()
-
-if needle != nil {
-  fmt.Printf("\nFound %d record(s) in %fs: %s (score: %1.2f)\n\n", len(needle), time_total, needle[0].Data, needle[0].Score)
-}
-```
-
-On my iMac it runs pretty fast
+On my iMac it runs pretty fast:
 
 ```
 Writing lorem ipsums to 'facts'
 
-2013/09/13 14:17:21 Wrote 483 words to 'Lorem Ipsums' (took 0.000425s)
-2013/09/13 14:17:21 Wrote 430 words to 'Lorem Ipsums' (took 0.000309s)
-2013/09/13 14:17:21 Wrote 307 words to 'Lorem Ipsums' (took 0.000188s)
-2013/09/13 14:17:21 Wrote 243 words to 'Lorem Ipsums' (took 0.000141s)
+2013/09/13 17:27:46 Wrote 483 words to 'Lorem Ipsums' (took 0.000343s)
+2013/09/13 17:27:46 Wrote 430 words to 'Lorem Ipsums' (took 0.000318s)
+2013/09/13 17:27:46 Wrote 307 words to 'Lorem Ipsums' (took 0.000207s)
+2013/09/13 17:27:46 Wrote 243 words to 'Lorem Ipsums' (took 0.000136s)
 
 Searching for 'a needle in a Hay Stack' in 'Lorem Ipsums' index
-Found 1 record(s) in 0.000001s: First Lorem Ipsum (score: 0.86)
-```
+Found 1 document(s) in 0.000001s: Document{uuid:
+54ff2ee3-1cbb-11e3-8bb7-fa8884c8312b, data: "First Lorem Ipsum", score:
+0.86}
 
 ## Todo
 * Write tests
--- src/phrase_search.go
+++ src/phrase_search.go
@@ -6,47 +6,55 @@ import (
 	"strings"
 	"sync"
 	"time"
+  "github.com/streadway/simpleuuid"
 )
 
-// A record has associated data and a score.
-// Scores are a percentage of phrase permutations over total sentence permutations
-//
-type Record struct {
-	Data  string
+type Token struct {
+  Document *Document
 	Score float32
 }
 
-// An index has a name, associated records, and can show debug output.
-//
-type Index struct {
-	Name    string
-	Records map[string][]*Record
-	Debug   bool
+type Document struct {
+  UUID simpleuuid.UUID
+  Data string
 }
 
-// Creates a new index.
-//
+type Index struct {
+	Name   string
+  Tokens map[string][]*Token
+	Debug  bool
+}
+
+type SearchResult struct {
+  TokenRef *Token
+  Document *Document
+}
+
+func (sr *SearchResult) Score() float32 {
+  return sr.TokenRef.Score
+}
+
+func NewDocument(data string) *Document {
+  uuid, _ := simpleuuid.NewTime(time.Now())
+  return &Document{uuid, data}
+}
+
 func NewIndex(name string, debug bool) *Index {
 	if debug {
 		log.Printf("Creating index '%s'", name)
 	}
-	return &Index{name, make(map[string][]*Record), debug}
+  return &Index{name, make(map[string][]*Token), debug}
 }
 
-// Generates nGram tokens and stores them in the index.
-//
-func (index *Index) Add(text string, data string) int {
+func (index *Index) Insert(text string, data string) int {
 	var wg sync.WaitGroup
 	var phrases []string
-	var wcounter int
 
-	// Init records
-	records := make(map[string]*Record)
-
-	// Split text into phrases
+  wcounter := 0
+  document := NewDocument(data)
+	token_map := make(map[string]*Token)
 	phrases = strings.FieldsFunc(text, SentenceDelims)
-
-	start := time.Now()
+	time_start := time.Now()
 
 	for _, phrase := range phrases {
 		phrase = strings.ToLower(phrase)
@@ -67,7 +75,7 @@ func (index *Index) Add(text string, data string) int {
 					score := float32(n) / float32(wc)
 					wcounter++
 
-					records[phrase] = &Record{data, score}
+					token_map[phrase] = &Token{document, score}
 					if index.Debug {
 						log.Printf("%s <-- %s : %s (perm_score=%d)", index.Name, phrase, data, score)
 					}
@@ -80,24 +88,20 @@ func (index *Index) Add(text string, data string) int {
 		wg.Wait()
 	}
 
-	total := time.Now().Sub(start)
-	log.Printf("Wrote %d words to '%s' (took %fs)", wcounter, index.Name, total.Seconds())
+	time_total := time.Now().Sub(time_start)
+	log.Printf("Wrote %d words to '%s' (took %fs)", wcounter, index.Name, time_total.Seconds())
 
-	for p, r := range records {
-		index.Records[p] = append(index.Records[p], r)
+	for p, r := range token_map {
+		index.Tokens[p] = append(index.Tokens[p], r)
 	}
 
-	return len(records)
+	return len(token_map)
 }
 
-// Looks up the phrase in an index's records
-//
-func (index *Index) Search(phrase string) []*Record {
-	return index.Records[strings.ToLower(phrase)]
+func (index *Index) Search(phrase string) []*Token {
+	return index.Tokens[strings.ToLower(phrase)]
 }
 
-// Sentnece delimiters
-//
 func SentenceDelims(r rune) bool {
 	return r == '.' || r == ',' || r == '?' || r == '!'
 }
@@ -106,10 +110,10 @@ func main() {
 	index := NewIndex("Lorem Ipsums", false)
 
 	fmt.Printf("\nWriting lorem ipsums to 'facts'\n\n")
-	index.Add("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. A needle in a hay stack", "First Lorem Ipsum")
-	index.Add("Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim.", "Second Loreum Ipsum")
-	index.Add("Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit.", "Third Loreum Ipsum")
-	index.Add("Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus, accumsan porttitor, cursus quis, aliquet eget, justo. ", "Fourth Loreum Ispum")
+	index.Insert("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. A needle in a hay stack", "First Lorem Ipsum")
+	index.Insert("Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim.", "Second Loreum Ipsum")
+	index.Insert("Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit.", "Third Loreum Ipsum")
+	index.Insert("Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus, accumsan porttitor, cursus quis, aliquet eget, justo. ", "Fourth Loreum Ispum")
 
 	fmt.Printf("\nSearching for 'a needle in a Hay Stack' in 'Lorem Ipsums' index")
 	time_start := time.Now()
@@ -117,6 +121,6 @@ func main() {
 	time_total := time.Now().Sub(time_start).Seconds()
 
 	if needle != nil {
-		fmt.Printf("\nFound %d record(s) in %fs: %s (score: %1.2f)\n\n", len(needle), time_total, needle[0].Data, needle[0].Score)
+    fmt.Printf("\nFound %d document(s) in %fs: Document{uuid: %s, data: \"%s\", score: %1.2f}\n\n", len(needle), time_total, needle[0].Document.UUID, needle[0].Document.Data, needle[0].Score)
 	}
 }
