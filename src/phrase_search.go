ac6acd18793d7daa1f6267825674835897733c2e
Eric Rafaloff <erafaloff@gmail.com>
2013-09-13 14:52:49 -0400

    Go Fmt

--- src/phrase_search.go
+++ src/phrase_search.go
@@ -49,7 +49,7 @@ func (index *Index) Add(text string, data string) int {
 	start := time.Now()
 
 	for _, phrase := range phrases {
-    phrase = strings.ToLower(phrase)
+		phrase = strings.ToLower(phrase)
 		words := strings.Split(phrase, " ")
 		wc := len(words)
 
@@ -111,12 +111,12 @@ func main() {
 	index.Add("Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit.", "Third Loreum Ipsum")
 	index.Add("Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus, accumsan porttitor, cursus quis, aliquet eget, justo. ", "Fourth Loreum Ispum")
 
-  fmt.Printf("\nSearching for 'a needle in a Hay Stack' in 'Lorem Ipsums' index")
-  time_start := time.Now()
-  needle := index.Search("a needle in a Hay Stack")
-  time_total := time.Now().Sub(time_start).Seconds()
+	fmt.Printf("\nSearching for 'a needle in a Hay Stack' in 'Lorem Ipsums' index")
+	time_start := time.Now()
+	needle := index.Search("a needle in a Hay Stack")
+	time_total := time.Now().Sub(time_start).Seconds()
 
-  if needle != nil {
-    fmt.Printf("\nFound %d record(s) in %fs: %s (score: %1.2f)\n\n", len(needle), time_total, needle[0].Data, needle[0].Score)
-  }
+	if needle != nil {
+		fmt.Printf("\nFound %d record(s) in %fs: %s (score: %1.2f)\n\n", len(needle), time_total, needle[0].Data, needle[0].Score)
+	}
 }
