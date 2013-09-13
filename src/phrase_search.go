e256b133e3a1081ec0759950bca1f55a7bd93728
Eric Rafaloff <erafaloff@gmail.com>
2013-09-13 13:51:21 -0400

    There we go! Making some progress

--- src/phrase_search.go
+++ src/phrase_search.go
@@ -14,7 +14,7 @@ import (
 //
 type Record struct {
 	Data  string
-	Score int
+	Score float32
 }
 
 // An index has a name, associated records, and can show debug output.
@@ -48,32 +48,26 @@ func (index *Index) Add(text string, data string) int {
   text = strings.ToLower(text)
 
 	// Split text into phrases
-  phrases = strings.Split(text, ".")
-  phrases = strings.Split(text, "!")
-  phrases = strings.Split(text, "?")
-  phrases = strings.Split(text, ",")
+  phrases = strings.FieldsFunc(text, SentenceDelims)
 
   start := time.Now()
-  log.Printf("Started writing %d phrases to '%s'", len(phrases), index.Name)
 
   for _, phrase := range phrases {
     words := strings.Split(phrase, " ")
 	  wc := len(words)
 
-    // If word count is 0, we have nothing to do
     if wc == 0 {
       break
     }
 
     wg.Add(wc)
 
-    // Generate nGram tokens for all cases of n and create associated records
     for n := wc; n > 0; n-- {
       go func(n int) {
         i_max := wc - (n - 1)
         for i := 0; i < i_max; i++ {
           phrase := strings.Join(words[i:i+n], " ")
-          score := wc - n
+          score := float32(n) / float32(wc)
           wcounter++
 
           records[phrase] = &Record{data, score}
@@ -90,7 +84,7 @@ func (index *Index) Add(text string, data string) int {
   }
 
   total := time.Now().Sub(start)
-  log.Printf("Wrote %d words (%fs)", wcounter, total.Seconds())
+  log.Printf("Wrote %d words to '%s' (took %fs)", wcounter, index.Name, total.Seconds())
 
   for p, r := range records {
     index.Records[p] = append(index.Records[p], r)
@@ -105,14 +99,21 @@ func (index *Index) Find(phrase string) []*Record {
 	return index.Records[strings.ToLower(phrase)]
 }
 
+// Sentnece delimiters
+//
+func SentenceDelims(r rune) bool {
+  return r == '.' || r == ',' || r == '?' || r == '!'
+}
+
 func main() {
 	index := NewIndex("facts", false)
 
   fmt.Printf("\nWriting lorem ipsum to 'facts'\n\n")
   index.Add("Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. A needle in a hay stack", "It works!")
   
+  start := time.Now()
   needle := index.Find("a needle in a Hay Stack")
   if needle != nil {
-    fmt.Printf("\nFinding 'a needle in a Hay Stack' Found %d record(s). Returned data: %s (score: %d)\n\n", len(needle), needle[0].Data, needle[0].Score)
+    fmt.Printf("\nFinding 'a needle in a Hay Stack' Found %d record(s) in %fs: %s (score: %1.2f)\n\n", len(needle), time.Now().Sub(start).Seconds(), needle[0].Data, needle[0].Score)
   }
 }
